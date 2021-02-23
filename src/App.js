6a95c41ad47cba62e4560d67c6f175504a9bcdf8
Avimistry4800 <avimistry4800@gmail.com>
2021-02-23 18:26:01 +0600

    added some heder and paragraph

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person ={
+    name : "Dr. MAhafuz",
+    job : "No 1 Singer in Bangladesh"
+  }
+ let person2 ={
+    name : "Eva Rahman",
+    job : "One and only kokil konthi Singer in Bangladesh"
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Here comes : {person.name}</h1>
+       <h2> {person.job}</h2>
+       <h3>{person2.name + ' : ' + person2.job}</h3>
+       <p>Rest of all is HISTORY</p>
+       <p>R.I.P. Bangladeshi people</p>
       </header>
     </div>
   );
